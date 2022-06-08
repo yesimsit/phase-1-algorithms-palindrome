0d34f6f65fd5aeef9784e31e0a2b05cbd427944c
yesim <sityesim@gmail.com>
2022-06-08 10:19:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  return word == word.split("").reverse().join("") ? true : false;
 }
-
+console.log(isPalindrome(mom))
 /* 
   Add your pseudocode here
 */
 
 /*
   Add written explanation of your solution here
+ if the word is equal with reversed word.It is palindrome.But if it is not equal, it is not a palindrome word.
+  and if it is not palindrome the result will be false , but if it is palendrome the result will be true
 */
 
 // You can run `node index.js` to view these console logs
